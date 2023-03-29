d0f70ea4174c8f63a643ba0e90edd4811b30e566
Sean Jung <sean.jung23@gmail.com>
2023-03-29 19:38:13 -0400

    Implemented functions in pseudoclassical/queue.js & pseudoclassical/stack.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.key = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.key++;
+  this.storage[this.key] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.key === 0) {
+    return;
+  } else {
+    var firstOut = this.storage[1];
+    delete this.storage[1];
+
+    if (this.key > 1) {
+      for (var i = 2; i <= this.key; i++) {
+        var currentStorageValue = this.storage[i];
+        var previousKey = i - 1;
+        delete this.storage[i];
+        this.storage[previousKey] = currentStorageValue;
+      }
+    }
+
+    this.key--;
+    return firstOut;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.key;
+};
