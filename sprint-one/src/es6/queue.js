14d07a43cf9cb79fbde6dbf4e74c2cb9d9f76428
Sean Jung <sean.jung23@gmail.com>
2023-03-29 19:52:01 -0400

    Implemented functions in es6/queue.js & es6/stack.js

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,37 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.key = 0;
   }
 
+  enqueue(value) {
+    this.key++;
+    this.storage[this.key] = value;
+  }
+
+  dequeue() {
+    if (this.key === 0) {
+      return;
+    } else {
+      var firstOut = this.storage[1];
+      delete this.storage[1];
+
+      if (this.key > 1) {
+        for (var i = 2; i <= this.key; i++) {
+          var currentStorageValue = this.storage[i];
+          var previousKey = i - 1;
+          delete this.storage[i];
+          this.storage[previousKey] = currentStorageValue;
+        }
+      }
+
+      this.key--;
+      return firstOut;
+    }
+  }
+
+  size() {
+    return this.key;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.key = 0;
   }
 
+  push(value) {
+    this.key++;
+    this.storage[this.key] = value;
+  }
+
+  pop() {
+    if (this.key === 0) {
+      return;
+    } else {
+      var top = this.storage[this.key];
+      delete this.storage[this.key];
+      this.key--;
+
+      return top;
+    }
+  }
+
+  size() {
+    return this.key;
+  }
 }
