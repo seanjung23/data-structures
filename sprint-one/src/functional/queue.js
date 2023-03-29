dc43fd840152ac9a198ec373a2ad9f8b2381626f
Sean Jung <sean.jung23@gmail.com>
2023-03-29 16:53:57 -0400

    Implemented functions in functional/queue.js and functional/stack.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var key = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,13 +8,33 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    key++;
+    storage[key] = value;
   };
 
   someInstance.dequeue = function() {
+    if (key === 0) {
+      return;
+    } else {
+      var top = storage[1];
+      delete storage[1];
+      if (key > 1) {
+        for (var i = 2; i <= key; i++) {
+          var currentStorageValue = storage[key];
+          var previousKey = i - 1;
+          storage[previousKey] = currentStorageValue;
+        }
+      }
+      key--;
+      return top;
+    }
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
 };
+
+//
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
   var someInstance = {};
+  var key = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    key++;
+    storage[key] = value;
   };
 
   someInstance.pop = function() {
+    if (key === 0) {
+      return;
+    } else {
+      var top = storage[key];
+      delete storage[key];
+      key--;
+      return top;
+    }
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
