e3cbc509f1a1448b3c46a8ee06e62bbe1b8f5a75
Sean Jung <sean.jung23@gmail.com>
2023-03-30 17:31:00 -0400

    Implemented LinkedList function in linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,20 +1,52 @@
+// Time Complexity: Constant
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
+  // Time Complexity: Constant
   list.addToTail = function(value) {
+    var node = Node(value);
+
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
+  // Time Complexity: Constant
   list.removeHead = function() {
+    var head = list.head;
+
+    if (head === null) {
+      return;
+    }
+    list.head = head.next;
+
+    return head.value;
   };
 
+  // Time Complexity: Linear
   list.contains = function(target) {
+    var node = list.head;
+
+    while (node !== null) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+
+    return false;
   };
 
   return list;
 };
 
+// Time Complexity: Constant
 var Node = function(value) {
   var node = {};
 
